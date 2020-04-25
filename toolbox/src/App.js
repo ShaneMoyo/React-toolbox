8b5d0bff96ea6873f117d2ea861a22e1f97706f8
Shane Moyo <smoyo@cvent.com>
2020-04-25 15:54:48 -0700

    getting gists and displaying detail.

--- toolbox/src/App.js
+++ toolbox/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import githubAPI from './services/github';
+import moment from 'moment'
 import './App.css';
 
 function App() {
   const [username, setUsername] = useState('');
   const [loading, setLoading] = useState('false');
   const [gists, setGists] = useState([]);
+  const [gistDetail, setGistDetail] = useState(null);
 
   const handleInput = ({ target }) => {
     console.log('input: ', target.value);
@@ -25,9 +27,31 @@ function App() {
       })
   }
 
+  const handleShowGistDetail = (key) => {
+    setGistDetail(key);
+  }
+
   const gistsList = gists.map((gist, index) => {
     console.log('gist, index ', gist, index)
-    return <li key={index}>{gist.id}</li>
+    const date = moment(gist.created_at). format('YYYY-MM-DD');
+    const files = gist.files;
+    const fileList = Object.entries(files).map((file, index) => {
+      return (
+        <li key={index} className="file">
+          <a href={file[1].raw_url}>{file[0]}</a>
+        </li>
+      )
+    })
+    return (
+      <li className="gist" key={index} onClick={e => handleShowGistDetail(index)}>
+        <a >{gist.description} - {date} </a>
+        {gistDetail ===  index ?
+          <ul>
+            {fileList}
+          </ul> : null
+        }
+      </li>
+      )
   })
 
 
